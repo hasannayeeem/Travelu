70e7b1f9c1a6327d6c0923d681b3394b8ca08662
hasannayeeem <hasannayeeem@gmail.com>
2022-04-19 23:43:35 +0600

    useServices hooks uptodate

---
+++ src/Hooks/useServices.js
@@ -0,0 +1,79 @@
+import React from 'react';
+
+const useServices = () => {
+    data= [
+        {
+            "id": 1,
+            "rating": 4.2,
+            "image": "https://bsmedia.business-standard.com/_media/bs/img/article/2020-06/06/full/1591446449-2606.png?im=Resize,width=640",
+            "comments": [
+                {
+                    "user": "messi",
+                    "text": "This is great. you are absolutely right. Don't stop!"
+                }
+            ]
+        },
+        {
+            "id": 6,
+            "rating": 4.3,
+            "image": "https://www.observerbd.com/2022/01/22/observerbd.com_1642871023.jpg",
+            "comments": [
+                {
+                    "user": "tamim iqbal",
+                    "text": "Twelve Minutes is a strange interactive thriller that’s better with friends"
+                }
+            ]
+        },
+        {
+            "id": 2,
+            "rating": 4.4,
+            "image": "https://c.ndtvimg.com/2019-10/kbn7tvqg_shakib-al-hasan_625x300_21_October_19.jpg?output-quality=80&downsize=1278:*",
+                  "comments": [
+                {
+                    "user": "sakib al hasan",
+                    "text": "Monster Hunter Rise is better on PC, but something’s missing some ❤."
+                }
+            ]
+        },
+        {
+            "id": 3,
+            "rating": 4.5,
+            "image": "https://images.unsplash.com/photo-1507003211169-0a1dd7228f2d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+            "comments": [
+                {
+                    "user": "john43",
+                    "text": "Kid A Mnesia Exhibition is an unsettling and beautiful Radiohead art exhibit"
+                }
+            ]
+        },
+        {
+            "id": 4,
+            "rating": 4.0,
+            "image": "https://cdn.dnaindia.com/sites/default/files/styles/full/public/2022/02/01/1018227-rcb.jpg",
+            "comments": [
+                {
+                    "user": "muhammad hafiz",
+                    "text": "Tunic is a Zelda-style adventure that rekindled my love of strategy guides"
+                }
+            ]
+        },
+        {
+            "id": 5,
+            "rating": 5.0,
+            "image": "https://i.guim.co.uk/img/media/f6e7abf29f374e97e6c602f34a320b2b1ec9b17b/122_261_3620_2173/master/3620.jpg?width=465&quality=45&auto=format&fit=max&dpr=2&s=cbef9da0b27456b42b61298e2699191e",
+            "comments": [
+                {
+                    "user": "muhammad salah",
+                    "text": "OlliOlli World captures the essence of skateboarding, and then makes it weird"
+                }
+            ]
+        }
+    ];
+    return (
+        <div>
+            
+        </div>
+    );
+};
+
+export default useServices;
